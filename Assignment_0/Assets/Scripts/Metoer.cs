2e5714f388c08f0e4aa7b5ba66bf9807a2abc0da
NiklasSorensen <niklass137@gmail.com>
2018-02-06 13:01:58 +0100

    using localScale for collision

--- Assignment_0/Assets/Scripts/Metoer.cs
+++ Assignment_0/Assets/Scripts/Metoer.cs
@@ -7,29 +7,49 @@ public class Metoer : MonoBehaviour {
 	public GameObject earthPrefab;
 	private Vector3 distance;
 	private float xScale;
-	private float zScale;
+
+	private float imageScaleX;
+	private float sunScaleX;
+	private float earthScaleX;
+	private float meteorScaleX;
+	private float image2ScaleX;
+
+	private float realXScale;
+
+	void Start(){
+		imageScaleX = earthPrefab.transform.parent.parent.localScale.x;
+		sunScaleX = earthPrefab.transform.parent.localScale.x;
+		earthScaleX = earthPrefab.transform.localScale.x;
+		meteorScaleX = transform.localScale.x;
+		image2ScaleX = transform.parent.localScale.x;
+
+		realXScale = ((imageScaleX * sunScaleX * earthScaleX) + (image2ScaleX * meteorScaleX))/2;
+
+	}
 
 	void Update(){
 		CalcDistance ();
 
 		if (Input.GetKey (KeyCode.Space)){
-			Debug.Log ("Distance " + distance.magnitude + " xScale : " + xScale + " zScale : " + zScale);
+			//Debug.Log ("Distance " + distance.magnitude + " xScale : " + xScale + " zScale : " + zScale);
+			Debug.Log ("real scale : " + realXScale);
+			Debug.Log ("lossy Scale : " + xScale);
 		}
 
 	}
 
 	void CalcDistance(){
 		xScale = (earthPrefab.transform.lossyScale.x + transform.lossyScale.x)/2;
-		zScale = (earthPrefab.transform.lossyScale.z + transform.lossyScale.z)/2;
+		//zScale = (earthPrefab.transform.lossyScale.z + transform.lossyScale.z)/2;
 		distance = transform.position - earthPrefab.transform.position;
 		//Debug.Log ("Distance " + distance + " xScale : " + xScale + " yScale : " + yScale);
-		if (Mathf.Abs(distance.magnitude) < xScale) {
+		if (Mathf.Abs(distance.magnitude) < realXScale) {
 			//Debug.Log ("Distance " + distance + " xScale : " + xScale + " zScale : " + zScale);
 			Debug.Log ("Collision");
 			//M
 			GetComponent<ParticleSystem>().Play();
 		}
-			
+
 
 	}
 }
