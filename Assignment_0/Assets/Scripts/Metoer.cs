b5be88a1588dd053a2fc020cf70f747a4c7677fd
NiklasSorensen <niklass137@gmail.com>
2018-02-06 11:30:03 +0100

    added project
    Assignment 0

---
+++ Assignment_0/Assets/Scripts/Metoer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metoer : MonoBehaviour {
+
+	public GameObject earthPrefab;
+	private Vector3 distance;
+	private float xScale;
+	private float zScale;
+
+	void Update(){
+		CalcDistance ();
+
+		if (Input.GetKey (KeyCode.Space)){
+			Debug.Log ("Distance " + distance.magnitude + " xScale : " + xScale + " zScale : " + zScale);
+		}
+
+	}
+
+	void CalcDistance(){
+		xScale = (earthPrefab.transform.lossyScale.x + transform.lossyScale.x)/2;
+		zScale = (earthPrefab.transform.lossyScale.z + transform.lossyScale.z)/2;
+		distance = transform.position - earthPrefab.transform.position;
+		//Debug.Log ("Distance " + distance + " xScale : " + xScale + " yScale : " + yScale);
+		if (Mathf.Abs(distance.magnitude) < xScale) {
+			//Debug.Log ("Distance " + distance + " xScale : " + xScale + " zScale : " + zScale);
+			Debug.Log ("Collision");
+			//M
+			GetComponent<ParticleSystem>().Play();
+		}
+			
+
+	}
+}
