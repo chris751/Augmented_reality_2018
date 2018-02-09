1086e48f15995c91145eef96fed04fac7a105c33
NiklasSorensen <niklass137@gmail.com>
2018-02-09 12:09:35 +0100

    commitAgain

--- Assignment_1/Assets/Landing.cs
+++ Assignment_1/Assets/Landing.cs
@@ -5,26 +5,31 @@ using UnityEngine;
 public class Landing : MonoBehaviour {
 
 	public GameObject shuttle;
-	public GameObject Landingstrip;
+	public GameObject landingstrip;
 
 	private Material quadColor;
+
+	private Vector3 shuttlePosition;
+	private Vector3 landingstripPosition;
+
 
 	// Use this for initialization
 	void Start () {
 		quadColor = GetComponent<Renderer>().material;
+
+		shuttlePosition = shuttle.transform.rotation;
+		landingstripPosition = landingstrip.transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		Debug.Log (Vector3.Dot (shuttlePosition, landingstripPosition));
+		if (Vector3.Dot (shuttlePosition, landingstripPosition) <= 1.2f && Vector3.Dot (shuttlePosition, landingstripPosition) >= 0.8) {
+			//quadColor.SetColor (_Color, Color.green);
+			quadColor.color = Color.green;
+		} else {
+			//quadColor.SetColor (_Color, Color.red);
+			quadColor.color = Color.red;
+		}
 	}
-
-	void ChangeColor() {
-		quadColor.SetColor (_Color, Color.red);
-		quadColor.SetColor (_Color, Color.green);
-
-
-	}
-
-
 }
---
+++ Assignment_1/Assets/PrintTransform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintTransform : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (gameObject.name + " rotation: " + transform.rotation);
+
+	}
+}
