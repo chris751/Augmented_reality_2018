45c78bc1e337442499ca9e31e4bdf97fe93882c3
NiklasSorensen <niklass137@gmail.com>
2018-02-09 10:58:50 +0100

    Dot.Product_SpaceShuttle

---
+++ Assignment_1/Assets/Landing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Landing : MonoBehaviour {
+
+	public GameObject shuttle;
+	public GameObject Landingstrip;
+
+	private Material quadColor;
+
+	// Use this for initialization
+	void Start () {
+		quadColor = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ChangeColor() {
+		quadColor.SetColor (_Color, Color.red);
+		quadColor.SetColor (_Color, Color.green);
+
+
+	}
+
+
+}
