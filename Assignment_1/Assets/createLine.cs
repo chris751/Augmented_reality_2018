be3e793f66698a36cd1ebb62fbcc9df4ac324cf3
nicolaile <nicolai.le2@gmail.com>
2018-03-09 10:53:50 +0100

    opgaveC

---
+++ Assignment_1/Assets/createLine.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createLine : MonoBehaviour
+{
+
+    GameObject line;
+    private LineRenderer lineObject;
+
+    private GameObject target1 = null;
+    private GameObject target2 = null;
+
+    //private LineRenderer lineObject;
+
+
+    // Use this for initialization
+    void Start()
+    {
+       line = new GameObject();
+           
+             line.AddComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Debug.Log(object1);
+
+
+
+        target1 = GameObject.FindGameObjectWithTag("Object1");
+        target2 = GameObject.FindGameObjectWithTag("Object2");
+
+           
+            lineObject = line.GetComponent<LineRenderer>();
+
+            lineObject.SetPosition(0, target1.transform.position);
+            lineObject.SetPosition(1, target2.transform.position);
+
+        
+    }
+
+}
---
+++ Assignment_1/Assets/distance.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class distance : MonoBehaviour {
+    public GameObject Cube2;
+    public GameObject Cube1;
+    private bool createLine;
+    // Use this for initialization
+    void Start () {
+       createLine = this.GetComponent<createLine>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       // createLine = this.GetComponent<createLine>().enabled = false;
+        if (Vector3.Distance(Cube1.transform.position, Cube2.transform.position) <30 && Vector3.Distance(Cube1.transform.position, Cube2.transform.position)!=21.5)
+        {
+            createLine = Cube1.GetComponent<createLine>().enabled = true;
+        }
+
+        Debug.Log(Vector3.Distance(Cube1.transform.position, Cube2.transform.position));
+
+
+    }
+}
